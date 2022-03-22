a068a5a513da1a4e8ad272cdd73c25e4f5e705f6
craned70 <craned70@gmail.com>
2022-03-21 19:42:41 -0700

    delete deprecated code
    todo: restart using bolt

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const { App } = require("@slack/bolt");
 require('dotenv').config();
 
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
-});
-
-(async () => {
-    await app.start(process.env.PORT || 3000);
-    console.log("It's Running!");
-})
+// Gotta use @slack/bolt
